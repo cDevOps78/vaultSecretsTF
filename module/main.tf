63d479c338ca83a35d441a74718b109fd74ade75
chaitu1 <chaitu143@gmail.com>
2024-04-05 23:23:22 +0530

    added automation to vault

--- module/main.tf
+++ module/main.tf
@@ -11,3 +11,22 @@ resource "vault_kv_secret" "secret" {
   path = "${vault_mount.kv.path}/${var.secret_m}"
   data_json = jsonencode("${var.secret_data}")
 }
+
+
+
+#resource "vault_mount" "kvv1" {
+#  path        = "kvv1"
+#  type        = "kv"
+#  options     = { version = "1" }
+#  description = "KV Version 1 secret engine mount"
+#}
+#
+#resource "vault_kv_secret" "secret" {
+#  path = "${vault_mount.kvv1.path}/secret"
+#  data_json = jsonencode(
+#    {
+#      zip = "zap",
+#      foo = "bar"
+#    }
+#  )
+#}
